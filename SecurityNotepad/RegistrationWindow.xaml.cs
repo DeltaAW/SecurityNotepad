117a7a5dce34a3296e00c34c0e70ac435b69f45a
DeltaAW <Ar.vagner@gmail.com>
2025-10-04 21:47:59 +0200

    first commit

---
+++ SecurityNotepad/RegistrationWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SecurityNotepad
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationWindow.xaml
+    /// </summary>
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+        private void PasswordBox_RegisterPassword(object sender, RoutedEventArgs e)
+        {
+            RegisterPassword.Visibility = string.IsNullOrEmpty(PasswordRegister.Password) ? Visibility.Visible : Visibility.Hidden;
+        }
+
+
+        private void TextBox_RegisterLogin(object sender, RoutedEventArgs e)
+        {
+            LoginBox.Visibility =
+                string.IsNullOrEmpty(LoginBox.Text) ? Visibility.Visible : Visibility.Hidden; // событие убирает текст в логине, когда пользователь начинает писать
+        }
+        public void Button_BackButton(object sender, RoutedEventArgs e)
+        {
+            EntryWindow entryWindow = new();
+            Close();
+            entryWindow.Show();
+        }  
+        public void Button_RegisterButton(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public void Button_ExiteButton(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ SecurityNotepad/entryWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using IOPath = System.IO.Path;
+
+namespace SecurityNotepad
+{
+    /// <summary>
+    /// Логика взаимодействия для entryWindow.xaml
+    /// </summary>
+    /// IncorrectPassOrLog.Visibility = Visibility.Visible; для вывода ложного пароля
+    public partial class EntryWindow : Window
+    {
+
+        public string DocumentsPath { get; private set; }
+        public string FolderPath { get; private set; }
+        public string FilePath { get; private set; }
+        public EntryWindow()
+        {
+            DocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); // получение пути к папке Document
+            FolderPath = IOPath.Combine(DocumentsPath, "NotepadAccount");
+            FilePath = IOPath.Combine(FolderPath, "Account.json");
+
+            InitializeComponent();
+            CreateFoldersForStrongAccounts();
+        }
+
+        public void CreateFoldersForStrongAccounts()
+        {
+            if (!CheckingForFileAvaliability())
+            {
+                Directory.CreateDirectory(FolderPath);
+                File.Create(FilePath);
+            }
+        }
+
+        public bool CheckingForFileAvaliability() => Directory.Exists(IOPath.Combine(DocumentsPath, "NotepadAccount"));
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordPlaceholder.Visibility =
+                string.IsNullOrEmpty(PasswordBox.Password) ? Visibility.Visible : Visibility.Hidden; // событие убирает текст в пароле, когда пользователь начинает писать
+        }
+
+        private void TextBox_LoginChanged(object sender, RoutedEventArgs e)
+        {
+            LoginPlaceholder.Visibility =
+                string.IsNullOrEmpty(Login.Text) ? Visibility.Visible : Visibility.Hidden; // событие убирает текст в логине, когда пользователь начинает писать
+        }
+
+        private void Button_LogIn(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Кнопка работает");
+        }
+
+        private void Button_SignUp(object sender, RoutedEventArgs e)
+        {
+            RegistrationWindow registrationWindow = new();
+            registrationWindow.Show();
+            Close();
+
+        }
+
+        private void Button_ExiteButton(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
